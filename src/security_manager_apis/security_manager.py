295565c2f1450ccfd39f81e60dbe80776b137e78
Bryan Ellis <bryan.ellis@firemon.com>
2023-11-21 13:20:58 -0600

    Update security_manager.py
    - update api methods to use private generalized api handler

--- src/security_manager_apis/security_manager.py
+++ src/security_manager_apis/security_manager.py
@@ -5,6 +5,61 @@ import authenticate_user
 from security_manager_apis.get_properties_data import get_properties_data
 
 
+def verify_route_json(route_input: dict):
+    """
+    Verifying validity of JSON
+    :param route_input: JSON passed
+    :return: 0
+    """
+    if 'interfaceName' in route_input and 'virtualRouter' in route_input:
+        raise Exception("Supplemental routes cannot use both an Interface and Virtual Router")
+    if 'interfaceName' not in route_input and route_input['virtualRouter'] == '':
+        raise Exception("Supplemental routes must use Interface or Virtual Router. JSON is missing value for "
+                        "virtualRouter.")
+    if 'virtualRouter' not in route_input and route_input['interfaceName'] == '':
+        raise Exception("Supplemental routes must use Interface or Virtual Router. JSON is missing value for "
+                        "interfaceName.")
+    return 0
+
+
+def mk_int(s: str) -> int:
+    """
+    Converting str to int, empty string returns None
+    :param s: string input
+    :return: int or None
+    """
+    s = s.strip()
+    return int(s) if s else None
+
+
+def build_route_json(line_input: list) -> dict:
+    """
+    Building JSON from Line input
+    :param line_input: Line from Text File
+    :return: JSON of supplemental route
+    """
+    if line_input[1] != '':
+        supp_route = {
+            "destination": line_input[2],
+            "deviceId": line_input[0],
+            "drop": json.loads(line_input[7].lower()),
+            "gateway": line_input[3],
+            "interfaceName": line_input[1],
+            "metric": mk_int(line_input[6])
+        }
+    else:
+        supp_route = {
+            "destination": line_input[2],
+            "deviceId": line_input[0],
+            "drop": json.loads(line_input[7].lower()),
+            "gateway": line_input[3],
+            "metric": mk_int(line_input[6]),
+            "nextVirtualRouter": line_input[5],
+            "virtualRouter": line_input[4]
+        }
+    return supp_route
+
+
 class SecurityManagerApis:
 
     def __init__(self, host: str, username: str, password: str, verify_ssl: bool, domain_id: str, suppress_ssl_warning=False):
@@ -15,25 +70,29 @@ class SecurityManagerApis:
         if suppress_ssl_warning:
             requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
         self.parser = get_properties_data()
-        self.api_instance = authenticate_user.Authentication(host, username, password, verify_ssl)
-        self.fm_api_session = self.api_instance.get_auth_token()
         self.host = host
-        self.api_resp = ''
+        self.fm_api_session = authenticate_user.Authentication(self.host, username, password, verify_ssl).get_auth_token()
         self.domain_id = domain_id
+
+    def __api_request(self, method: str, endpoint: str, payload=None, parameters=None, data=None, timeout=None, files=None):
+        try:
+            resp = self.fm_api_session.request(method, endpoint, json=payload, params=parameters, data=data, timeout=timeout, files=files)
+            resp.raise_for_status()
+            return resp
+        except requests.exceptions.HTTPError:
+            raise
+        except requests.exceptions.Timeout:
+            raise
 
     def get_devices(self) -> dict:
         """
         Method to retrieve devices from Security Manager
         """
         endpoint = self.parser.get('REST', 'get_dev_sm_api').format(self.host, self.domain_id)
-        try:
-            resp = self.fm_api_session.get(url=endpoint)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred retrieving devices: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint)
+        return resp.json()
 
-    def manual_device_retrieval(self, device_id: str) -> int:
+    def manual_device_retrieval(self, device_id: str):
         """
         Method to execute manual device retrieval
         :device_id: Device ID
@@ -41,12 +100,8 @@ class SecurityManagerApis:
         """
         endpoint = self.parser.get('REST', 'man_ret_dev_sm_api').format(self.host, self.domain_id, device_id)
         payload = {}
-        try:
-            resp = self.fm_api_session.post(url=endpoint, json=payload)
-            resp.raise_for_status()
-            return resp.status_code
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred executing manual device retrieval: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('POST', endpoint, payload)
+        return resp
 
     def siql_query(self, query_type: str, query: str, page_size: int) -> dict:
         """
@@ -58,12 +113,8 @@ class SecurityManagerApis:
         """
         endpoint = self.parser.get('REST', 'siql_query_sm_api').format(self.host, query_type)
         parameters = {'q': query, 'pageSize': page_size}
-        try:
-            resp = self.fm_api_session.get(url=endpoint, params=parameters)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred executing SIQL query: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint, None, parameters)
+        return resp.json()
 
     def create_device_group(self, device_group_name: str):
         """
@@ -73,12 +124,8 @@ class SecurityManagerApis:
         """
         endpoint = self.parser.get('REST', 'create_device_group').format(self.host, self.domain_id)
         payload = {'name': device_group_name, 'domainId': self.domain_id}
-        try:
-            resp = self.fm_api_session.post(url=endpoint, json=payload)
-            resp.raise_for_status()
-            return resp
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred creating device group: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('POST', endpoint, payload)
+        return resp
 
     def add_to_device_group(self, device_group_id: str, device_id: str):
         """
@@ -88,12 +135,8 @@ class SecurityManagerApis:
         :return: Response object
         """
         endpoint = self.parser.get('REST', 'add_device_to_group').format(self.host, self.domain_id, device_group_id, device_id)
-        try:
-            resp = self.fm_api_session.post(url=endpoint)
-            resp.raise_for_status()
-            return resp
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred adding device to device group: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('POST', endpoint)
+        return resp
 
     def get_device_group_by_name(self, device_group_name: str):
         """
@@ -102,12 +145,8 @@ class SecurityManagerApis:
         :return: Response object
         """
         endpoint = self.parser.get('REST', 'get_device_group_name').format(self.host, self.domain_id, device_group_name)
-        try:
-            resp = self.fm_api_session.get(url=endpoint)
-            resp.raise_for_status()
-            return resp
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred retrieving device group: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint)
+        return resp
 
     def zone_search(self, device_id: str, page_size: int) -> dict:
         """
@@ -118,12 +157,8 @@ class SecurityManagerApis:
         """
         endpoint = self.parser.get('REST', 'zone_search_sm_api').format(self.host, self.domain_id, device_id)
         parameters = {'pageSize': page_size}
-        try:
-            resp = self.fm_api_session.get(url=endpoint, params=parameters)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred while retrieving zones: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint, None, parameters)
+        return resp.json()
 
     def get_fw_obj(self, obj_type: str, device_id: str, match_id: str) -> dict:
         """
@@ -134,12 +169,8 @@ class SecurityManagerApis:
         :return: Firewall object JSON
         """
         endpoint = self.parser.get('REST', 'fw_obj_sm_api').format(self.host, obj_type, device_id, match_id)
-        try:
-            resp = self.fm_api_session.get(url=endpoint)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred while retrieving firewall object: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint)
+        return resp.json()
 
     def get_device_obj(self, device_id: str) -> dict:
         """
@@ -148,12 +179,8 @@ class SecurityManagerApis:
         :return: Device object JSON
         """
         endpoint = self.parser.get('REST', 'dev_obj_sm_api').format(self.host, self.domain_id, device_id)
-        try:
-            resp = self.fm_api_session.get(url=endpoint)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred while retrieving device object: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint)
+        return resp.json()
 
     def add_supp_route(self, device_id: str, supplemental_route: dict):
         """
@@ -162,14 +189,10 @@ class SecurityManagerApis:
         :param supplemental_route: JSON of Supplemental Route
         :return: List containing status code, reason, json
         """
-        self.verify_route_json(supplemental_route)
+        verify_route_json(supplemental_route)
         endpoint = self.parser.get('REST', 'supp_route_sm_api').format(self.host, device_id)
-        try:
-            resp = self.fm_api_session.post(url=endpoint, json=supplemental_route)
-            resp.raise_for_status()
-            return [resp.status_code, resp.reason, resp.json()]
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred while adding supplemental route: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('POST', endpoint, supplemental_route)
+        return resp
 
     def get_rule_doc(self, device_id: str, rule_id: str) -> dict:
         """
@@ -179,14 +202,10 @@ class SecurityManagerApis:
         :return: JSON response
         """
         endpoint = self.parser.get('REST', 'get_rule_doc').format(self.host, self.domain_id, device_id, rule_id)
-        try:
-            resp = self.fm_api_session.get(url=endpoint)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred retrieving rule documentation: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint)
+        return resp.json()
 
-    def update_rule_doc(self, device_id: str, rule_doc: dict) -> list:
+    def update_rule_doc(self, device_id: str, rule_doc: dict):
         """
         Method to update rule documentation
         :param device_id: ID of device
@@ -194,12 +213,8 @@ class SecurityManagerApis:
         :return: JSON response code and reason as list
         """
         endpoint = self.parser.get('REST', 'update_rule_doc').format(self.host, self.domain_id, device_id)
-        try:
-            resp = self.fm_api_session.put(url=endpoint, json=rule_doc)
-            resp.raise_for_status()
-            return [resp.status_code, resp.reason]
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred updating rule documentation: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('PUT', endpoint, rule_doc)
+        return resp
 
     def get_all_users(self, page_size=20) -> dict:
         """
@@ -208,12 +223,8 @@ class SecurityManagerApis:
         :return: JSON response
         """
         endpoint = self.parser.get('REST', 'get_all_users').format(self.host, self.domain_id, page_size)
-        try:
-            resp = self.fm_api_session.get(url=endpoint)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred retrieving all users: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint)
+        return resp.json()
 
     def get_user_by_username(self, username: str, page_size=20) -> dict:
         """
@@ -223,12 +234,8 @@ class SecurityManagerApis:
         :return: JSON response
         """
         endpoint = self.parser.get('REST', 'user_by_username').format(self.host, self.domain_id, page_size, username)
-        try:
-            resp = self.fm_api_session.get(url=endpoint)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred retrieving user: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint)
+        return resp.json()
 
     def get_user_groups(self, page_size=20) -> dict:
         """
@@ -237,12 +244,8 @@ class SecurityManagerApis:
         :return: JSON response
         """
         endpoint = self.parser.get('REST', 'get_user_groups').format(self.host, self.domain_id, page_size)
-        try:
-            resp = self.fm_api_session.get(url=endpoint)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred retrieving all user groups: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint)
+        return resp.json()
 
     def get_users_in_user_group(self, user_group_id: str, page_size=20) -> dict:
         """
@@ -252,12 +255,8 @@ class SecurityManagerApis:
         :return: JSON response
         """
         endpoint = self.parser.get('REST', 'get_users_in_user_groups').format(self.host, self.domain_id, user_group_id, page_size)
-        try:
-            resp = self.fm_api_session.get(url=endpoint)
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred retrieving all users in user group: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('GET', endpoint)
+        return resp.json()
 
     def add_user_to_user_group(self, user_group_id: str, user_id: str):
         """
@@ -267,25 +266,17 @@ class SecurityManagerApis:
         :return: Response object
         """
         endpoint = self.parser.get('REST', 'assign_to_user_group').format(self.host, self.domain_id, user_group_id, user_id)
-        try:
-            resp = self.fm_api_session.post(url=endpoint)
-            resp.raise_for_status()
-            return resp
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred retrieving adding user to user group: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('POST', endpoint)
+        return resp
 
     def logout(self) -> list:
         """
         Method to logout of current session
         """
-        self.fm_api_session.headers.update({'Connection': 'Close'})
+        self.fm_api_session.headers['Connection'] = 'Close'
         endpoint = self.parser.get('REST', 'logout_api_url').format(self.host)
-        try:
-            resp = self.fm_api_session.post(url=endpoint)
-            resp.raise_for_status()
-            return [resp.status_code, resp.reason]
-        except requests.exceptions.HTTPError as e:
-            raise SystemExit("Exception occurred logging out of session: {0}\nMessage: {1}".format(e, e.response.text))
+        resp = self.__api_request('POST', endpoint)
+        return resp
 
     def bulk_add_supp_route(self, f) -> int:
         """
@@ -301,59 +292,7 @@ class SecurityManagerApis:
                 line_count += 1
             else:
                 line_count += 1
-                upload = self.add_supp_route(row[0], self.build_route_json(row))
+                upload = self.add_supp_route(row[0], build_route_json(row))
                 print("Line " + str(line_count) + ":", upload[0], upload[1])
         print(f'Processed {line_count} lines.')
-        return 0
-
-    def mk_int(self, s: str) -> int:
-        """
-        Converting str to int, empty string returns None
-        :param s: string input
-        :return: int or None
-        """
-        s = s.strip()
-        return int(s) if s else None
-
-    def build_route_json(self, line_input: list) -> dict:
-        """
-        Building JSON from Line input
-        :param line_input: Line from Text File
-        :return: JSON of supplemmental route
-        """
-        if line_input[1] != '':
-            supp_route = {
-                "destination": line_input[2],
-                "deviceId": line_input[0],
-                "drop": json.loads(line_input[7].lower()),
-                "gateway": line_input[3],
-                "interfaceName": line_input[1],
-                "metric": self.mk_int(line_input[6])
-            }
-        else:
-            supp_route = {
-                "destination": line_input[2],
-                "deviceId": line_input[0],
-                "drop": json.loads(line_input[7].lower()),
-                "gateway": line_input[3],
-                "metric": self.mk_int(line_input[6]),
-                "nextVirtualRouter": line_input[5],
-                "virtualRouter": line_input[4]
-            }
-        return supp_route
-
-    def verify_route_json(self, route_input: dict):
-        """
-        Verifying validity of JSON
-        :param route_input: JSON passed
-        :return: 0
-        """
-        if 'interfaceName' in route_input and 'virtualRouter' in route_input:
-            raise Exception("Supplemental routes cannot use both an Interface and Virtual Router")
-        if 'interfaceName' not in route_input and route_input['virtualRouter'] == '':
-            raise Exception("Supplemental routes must use Interface or Virtual Router. JSON is missing value for "
-                            "virtualRouter.")
-        if 'virtualRouter' not in route_input and route_input['interfaceName'] == '':
-            raise Exception("Supplemental routes must use Interface or Virtual Router. JSON is missing value for "
-                            "interfaceName.")
         return 0
